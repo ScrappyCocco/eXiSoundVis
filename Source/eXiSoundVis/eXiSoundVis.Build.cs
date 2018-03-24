2ec6b5f12622c2ada72a332d0183c411252e8b25
ScrappyCocco <mikelerogeratro@yahoo.it>
2018-03-24 12:13:22 +0100

    Plugin Update to 4.19

--- Source/eXiSoundVis/eXiSoundVis.Build.cs
+++ Source/eXiSoundVis/eXiSoundVis.Build.cs
@@ -1,26 +1,29 @@
-using UnrealBuildTool;
-using System.IO;
+//using UnrealBuildTool; not necessary apparently
+//using System.IO; not necessary apparently
 
-public class eXiSoundVis : ModuleRules
+namespace UnrealBuildTool.Rules
 {
-	public eXiSoundVis(ReadOnlyTargetRules Target)
-	{
-		PrivateIncludePaths.AddRange(new string[] {	"eXiSoundVis/Private" });
-		PublicIncludePaths.AddRange(new string[] { "eXiSoundVis/Public" });
-		
-		PublicDependencyModuleNames.AddRange(new string[] { "Engine", "Core", "CoreUObject", "InputCore", "RHI", "Kiss_FFT" });
-
-        if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32)
+    public class eXiSoundVis : ModuleRules
+    {
+        public eXiSoundVis(ReadOnlyTargetRules Target) : base(Target)
         {
-            // VS2015 updated some of the CRT definitions but not all of the Windows SDK has been updated to match.
-            // Microsoft provides this shim library to enable building with VS2015 until they fix everything up.
-            //@todo: remove when no longer neeeded (no other code changes should be necessary).
-            if (WindowsPlatform.Compiler == WindowsCompiler.VisualStudio2015)
-            {
-                PublicAdditionalLibraries.Add("legacy_stdio_definitions.lib");
-            }
-        }
+            PrivateIncludePaths.AddRange(new string[] { "eXiSoundVis/Private" });
+            PublicIncludePaths.AddRange(new string[] { "eXiSoundVis/Public" });
 
-        AddThirdPartyPrivateStaticDependencies(Target, "Kiss_FFT");
-	}
+            PublicDependencyModuleNames.AddRange(new string[] { "Engine", "Core", "CoreUObject", "InputCore", "RHI", "Kiss_FFT" });
+
+            if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32)
+            {
+                // VS2015 updated some of the CRT definitions but not all of the Windows SDK has been updated to match.
+                // Microsoft provides this shim library to enable building with VS2015 until they fix everything up.
+                //@todo: remove when no longer neeeded (no other code changes should be necessary).
+                if (Target.WindowsPlatform.bNeedsLegacyStdioDefinitionsLib)
+                {
+                    PublicAdditionalLibraries.Add("legacy_stdio_definitions.lib");
+                }
+            }
+
+            AddEngineThirdPartyPrivateStaticDependencies(Target, "Kiss_FFT");
+        }
+    }
 }
